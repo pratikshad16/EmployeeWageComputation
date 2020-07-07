0d28efd3eaa4a20c770785aa261c6178289b38cb
pratikshad16 <dhagepratiksha16@gmail.com>
2020-07-07 14:49:57 +0530

    Added:functionality to store the daily wage with total wage

--- empwage.sh
+++ empwage.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x
 echo "Welcome to employee wage computation program"
+#constants
 wagePerHours=20
-#workingHours=8
-#partTimeHours=4
 workingDaysPerMonth=20
 absent=0
 fullTime=1
@@ -11,6 +10,10 @@ partTime=2
 totalWage=0
 totalWorkingHrs=0
 totalWorkingDays=0
+count=0
+declare -A dailyWage
+declare -A totalWage
+
 function getWorkHours () {
 	local checkEmp=$1
 	case $checkEmp in
@@ -29,10 +32,19 @@ echo $empHrs
 while [[ $totalWorkingHrs -lt 100 && $totalWorkingDays -lt 20 ]]
 do
 	isPresent=$(( RANDOM % 3  ))
-	((totalWorkingDays++))
 	workingHrs=$( getWorkHours $isPresent )
-	dailyWage=$(( $wagePerHours * $workingHrs ))
-	totalWage=$(( $totalWage + $dailyWage ))
+	dailWage[totalWorkingDays]=$(( $wagePerHours * $workingHrs ))
+	totalWage=$(( $totalWage + ($wagePerHours * $workingHrs)  ))
+	totalWage[$totalWorkingDays]=$totalWage
 	totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
+	((totalWorkingDays++))
 done
+for (( i=0; i<$totalWorkingDays; i++ ))
+do
+	echo ${dailyWage[$i]}" "${totalWage[$i]}
+done
+
 echo $totalWage
+echo ${dailyWage[@]}
+
+echo ${totalWage[@]}
